8db22d44f8f5798ba81c0278437e303501015fd2
Nick Kokkos <nkokkos@gmail.com>
2009-09-13 15:37:19 +0300

    Added the comments model

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090910123439) do
+ActiveRecord::Schema.define(:version => 20090913123509) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "name"
