edee397f9c43a3aa1c1956fbbac9cb252b46d7c7
Nick Kokkos <nkokkos@gmail.com>
2009-09-21 15:53:22 +0300

    Added migration to add language field to comments

---
+++ db/migrate/20090921125024_add_language_option_to_comments.rb
@@ -0,0 +1,9 @@
+class AddLanguageOptionToComments < ActiveRecord::Migration
+  def self.up
+    add_column :comments, :language, :string
+  end
+
+  def self.down
+    remove_column :comments, :language
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20090921125024) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "language"
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
