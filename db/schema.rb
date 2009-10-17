70a87d5da964690ccfb712dec69681cead1265be
Nick Kokkos <nkokkos@gmail.com>
2009-10-17 15:15:05 +0300

    <

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,30 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20090921131108) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "language"
+  end
+
+  create_table "languages", :force => true do |t|
+    t.string   "name"
+    t.string   "language_code"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
